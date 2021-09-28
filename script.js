d4e3a460209d4a4f9b8624d391296b0db6ba307b
Micolomike <91351725+Micolomike@users.noreply.github.com>
2021-09-29 00:25:05 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -48.785, lng: 2.607 },
+    center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
 }
